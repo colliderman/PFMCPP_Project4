e984617391c3a07ffbbdac3cd28f20dc71399c15
colliderman <cody@empresseffects.com>
2020-02-26 03:49:22 +0000

    Completed task

--- main.cpp
+++ main.cpp
@@ -36,9 +36,124 @@ send me a DM to check your pull request
 
  Wait for my code review.
  */
-
 #include <iostream>
+
+struct floatType
+{
+    float add ( float lhs, float rhs );
+    float subtract ( float lhs, float rhs );
+    float multiply ( float lhs, float rhs );
+    float divide ( float lhs, float rhs );
+};
+
+float floatType::add ( float lhs, float rhs )
+{
+    return lhs + rhs;
+}
+
+float floatType::subtract ( float lhs, float rhs )
+{
+    return lhs - rhs;
+}
+
+float floatType::multiply ( float lhs, float rhs )
+{
+    return lhs * rhs;
+}
+
+float floatType::divide ( float lhs, float rhs )
+{
+    return lhs / rhs;
+}
+
+struct doubleType
+{
+    double add ( double lhs, double rhs );
+    double subtract ( double lhs, double rhs );
+    double multiply ( double lhs, double rhs );
+    double divide ( double lhs, double rhs );
+};
+
+double doubleType::add ( double lhs, double rhs )
+{
+    return lhs + rhs;
+}
+
+double doubleType::subtract ( double lhs, double rhs )
+{
+    return lhs - rhs;
+}
+
+double doubleType::multiply ( double lhs, double rhs )
+{
+    return lhs * rhs;
+}
+
+double doubleType::divide ( double lhs, double rhs )
+{
+    return lhs / rhs;
+}
+
+struct intType
+{
+    int add ( int lhs, int rhs );
+    int subtract ( int lhs, int rhs );
+    int multiply ( int lhs, int rhs );
+    int divide ( int lhs, int rhs );
+};
+
+int intType::add ( int lhs, int rhs )
+{
+    return lhs + rhs;
+}
+
+int intType::subtract ( int lhs, int rhs )
+{
+    return lhs - rhs;
+}
+
+int intType::multiply ( int lhs, int rhs )
+{
+    return lhs * rhs;
+}
+
+int intType::divide ( int lhs, int rhs )
+{
+    if ( rhs != 0 )
+        return lhs / rhs;
+
+    std::cout << "can't divide integer by zero!";
+    return 0;    
+}
+
 int main()
 {
+    floatType ft;
+    float floatParamLHS = 13.6f;
+    float floatParamRHS = 22.9f;
+
+    doubleType dt;
+    double doubleParamLHS = 42.673;
+    double doubleParamRHS = 3.72;
+
+    intType it;
+    int intParamLHS = 48;
+    int intParamRHS = 16;
+
+    std::cout << "Result of ft.add() is " << ft.add ( floatParamLHS, floatParamRHS ) << std::endl;
+    std::cout << "Result of ft.subtract is " << ft.subtract ( floatParamLHS, floatParamRHS ) << std::endl;
+    std::cout << "Result of ft.multiply() is " << ft.multiply ( floatParamLHS, floatParamRHS ) << std::endl;
+    std::cout << "Result of ft.divide() is " << ft.divide ( floatParamLHS, floatParamRHS ) << std::endl;
+
+    std::cout << "Result of dt.add() is " << dt.add ( doubleParamLHS, doubleParamRHS ) << std::endl;
+    std::cout << "Result of dt.subtract is " << dt.subtract ( doubleParamLHS, doubleParamRHS ) << std::endl;
+    std::cout << "Result of dt.multiply() is " << dt.multiply ( doubleParamLHS, doubleParamRHS ) << std::endl;
+    std::cout << "Result of dt.divide() is " << dt.divide ( doubleParamLHS, doubleParamRHS ) << std::endl;
+
+    std::cout << "Result of it.add() is " << it.add ( intParamLHS, intParamRHS ) << std::endl;
+    std::cout << "Result of it.subtract is " << it.subtract ( intParamLHS, intParamRHS ) << std::endl;
+    std::cout << "Result of it.multiply() is " << it.multiply ( intParamLHS, intParamRHS ) << std::endl;
+    std::cout << "Result of it.divide() is " << it.divide ( intParamLHS, intParamRHS ) << std::endl;
+
     std::cout << "good to go!" << std::endl;
 }
